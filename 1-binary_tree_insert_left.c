a97b96f33918779010893c27d745c1a32e84767e
mutantEris <mmires@live.com>
2022-04-08 00:04:03 -0500

    betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,19 +9,19 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t * node;
+	binary_tree_t *node;
 
 	if (!parent)
 		return (NULL);
 	node = malloc(sizeof(binary_tree_t));
-	if (! node)
+	if (!node)
 		return (NULL);
 	node->parent = parent;
 	node->n = value;
 	node->left = NULL;
 	node->right = NULL;
 	if (parent->left)
-	{
+    {
 	    node->left = parent->left;
 	    parent->left->parent = node;
 	}
