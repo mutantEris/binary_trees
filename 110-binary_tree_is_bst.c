6d7736803dc2ae4f8d4c6da3e1f13c8e8eb5d42c
mutantEris <mmires@live.com>
2022-04-15 00:07:13 -0500

    swap

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -2,6 +2,18 @@
 #include <limits.h>
 
 /**
+ * binary_tree_is_bst - Determines Search Treeness
+ * @tree: Tree Root Pointer
+ * Return: 1 if tree is bst otherwise 0
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (bstHelper(tree, INT_MIN, INT_MAX));
+}
+
+/**
  * bstHelper - Is it bts?
  * @node: Node
  * @min: Minimum
@@ -19,16 +31,4 @@ int bstHelper(const binary_tree_t *node, int min, int max)
 		return (0);
 	return (bstHelper(node->left, min, node->n - 1)
 		&& bstHelper(node->right, node->n + 1, max));
-}
-
-/**
- * binary_tree_is_bst - Determines Search Treeness
- * @tree: Tree Root Pointer
- * Return: 1 if tree is bst otherwise 0
- */
-int binary_tree_is_bst(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-	return (bstHelper(tree, INT_MIN, INT_MAX));
 }
